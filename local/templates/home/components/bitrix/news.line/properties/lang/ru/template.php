d4f5897f6362324331c0f5033c853112a4412f6e
Voloshin-Sergei <4voloshin@gmail.com>
2023-03-06 22:51:19 +0300

    fix: gitignore

---
+++ local/templates/home/components/bitrix/news.line/properties/lang/ru/template.php
@@ -0,0 +1,8 @@
+<?
+$MESS["CT_BNL_ELEMENT_DELETE_CONFIRM"] = "Будет удалена вся информация, связанная с этой записью. Продолжить?";
+$MESS["PROPERTIES_TITLE"] = "Новая недвижимость для вас";
+$MESS["AREA"] = "Площадь";
+$MESS["FLOORS"] = "Этажи";
+$MESS["BATHS"] = "Ванны";
+$MESS["GARAGE"] = "Гараж";
+?>
---
+++ local/templates/home/components/bitrix/news.line/properties/template.php
@@ -0,0 +1,85 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+  die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+
+<div class="row mb-5">
+      <div class="col-12">
+        <div class="site-section-title">
+          <h2><?=GetMessage("PROPERTIES_TITLE")?></h2>
+        </div>
+      </div>
+    </div>
+
+<div class="row mb-5">
+
+  <? foreach ($arResult["ITEMS"] as $arItem): ?>
+    <?
+    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+    ?>
+
+<div id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="col-md-6 col-lg-4 mb-4">
+      <a href="#" class="prop-entry d-block">
+        <figure> <img alt="Image" src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>" class="img-fluid"> </figure>
+        <div class="prop-text">
+          <div class="inner">
+            <span class="price rounded">$
+              <? echo $arItem["PROPERTY_PRICE_VALUE"] ?>
+            </span>
+            <h3 class="title">
+              <? echo $arItem["NAME"] ?>
+            </h3>
+            <p class="location">
+              <? echo $arItem["NAME"] ?>
+            </p>
+          </div>
+          <div class="prop-more-info">
+            <div class="inner d-flex">
+              <div class="col">
+              <?=GetMessage("AREA")?>: <strong>
+                  <? echo $arItem["PROPERTY_TOTAL_AREA_VALUE"] ?>m<sup>
+                    2
+                  </sup>
+                </strong>
+              </div>
+              <div class="col">
+              <?=GetMessage("FLOORS")?>: <strong>
+                  <? echo $arItem["PROPERTY_FLOORS_VALUE"] ?>
+                </strong>
+              </div>
+              <div class="col">
+              <?=GetMessage("BATHS")?>: <strong>
+                  <? echo $arItem["PROPERTY_BATHROOMS_VALUE"] ?>
+                </strong>
+              </div>
+              <div class="col">
+              <?=GetMessage("GARAGE")?>: <strong>
+                  <? echo $arItem["PROPERTY_GARAGE_VALUE"] ?: "нет"; ?>
+
+                </strong>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+
+    
+
+  <? endforeach; ?>
+
+
+
+</div>
