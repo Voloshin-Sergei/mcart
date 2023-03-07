43569b5152293c69c170e979250b70f6371ffb2a
Voloshin-Sergei <4voloshin@gmail.com>
2023-03-07 13:50:39 +0300

    fix: Task 3 поправил корректное отображение верхнего меню

--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -1,4 +1,5 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+  die();
 
 IncludeTemplateLangFile(__FILE__);
 
@@ -40,16 +41,18 @@ $asset->addJs(SITE_TEMPLATE_PATH . "/js/main.js");
 ?>
 
 <!DOCTYPE html>
-<html lang="<?=LANGUAGE_ID?>">
+<html lang="<?= LANGUAGE_ID ?>">
 
 <head>
-    <title><?$APPLICATION->ShowTitle()?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <?$APPLICATION->ShowHead();?>
+  <title>
+    <? $APPLICATION->ShowTitle() ?>
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <? $APPLICATION->ShowHead(); ?>
 </head>
 
 <body>
-<?$APPLICATION->ShowPanel();?>
+  <? $APPLICATION->ShowPanel(); ?>
   <div class="site-loader"></div>
 
   <div class="site-wrap">
@@ -69,42 +72,42 @@ $asset->addJs(SITE_TEMPLATE_PATH . "/js/main.js");
           <div class="col-6 col-md-6">
             <p class="mb-0">
 
-            <?$APPLICATION->IncludeComponent(
-	            "bitrix:main.include",
-	            "",
-	            Array(
-		          "AREA_FILE_SHOW" => "file",
-		          "AREA_FILE_SUFFIX" => "inc",
-		          "EDIT_TEMPLATE" => "",
-		          "PATH" => "/include/header/phone.php"
-	            )
-            );?>
-              
-              <?$APPLICATION->IncludeComponent(
-	              "bitrix:main.include",
-	              "",
-	              Array(
-	              	"AREA_FILE_SHOW" => "file",
-	              	"AREA_FILE_SUFFIX" => "inc",
-	              	"EDIT_TEMPLATE" => "",
-	              	"PATH" => "/include/header/email.php"
-	              )
-                );?>
+              <? $APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                "",
+                array(
+                  "AREA_FILE_SHOW" => "file",
+                  "AREA_FILE_SUFFIX" => "inc",
+                  "EDIT_TEMPLATE" => "",
+                  "PATH" => "/include/header/phone.php"
+                )
+              ); ?>
+
+              <? $APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                "",
+                array(
+                  "AREA_FILE_SHOW" => "file",
+                  "AREA_FILE_SUFFIX" => "inc",
+                  "EDIT_TEMPLATE" => "",
+                  "PATH" => "/include/header/email.php"
+                )
+              ); ?>
 
             </p>
           </div>
           <div class="col-6 col-md-6 text-right">
 
-              <?$APPLICATION->IncludeComponent(
-	              "bitrix:main.include",
-	              "",
-	              Array(
-	              	"AREA_FILE_SHOW" => "file",
-	              	"AREA_FILE_SUFFIX" => "inc",
-	              	"EDIT_TEMPLATE" => "",
-	              	"PATH" => "/include/header/social.php"
-	              )
-              );?>
+            <? $APPLICATION->IncludeComponent(
+              "bitrix:main.include",
+              "",
+              array(
+                "AREA_FILE_SHOW" => "file",
+                "AREA_FILE_SUFFIX" => "inc",
+                "EDIT_TEMPLATE" => "",
+                "PATH" => "/include/header/social.php"
+              )
+            ); ?>
 
           </div>
         </div>
@@ -117,50 +120,50 @@ $asset->addJs(SITE_TEMPLATE_PATH . "/js/main.js");
           <div class="col-8 col-md-8 col-lg-4">
             <h1 class="">
 
-            <?$APPLICATION->IncludeComponent(
-	              "bitrix:main.include",
-	              "",
-	              Array(
-	              	"AREA_FILE_SHOW" => "file",
-	              	"AREA_FILE_SUFFIX" => "inc",
-	              	"EDIT_TEMPLATE" => "",
-	              	"PATH" => "/include/header/logo.php"
-	              )
-              );?>
+              <? $APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                "",
+                array(
+                  "AREA_FILE_SHOW" => "file",
+                  "AREA_FILE_SUFFIX" => "inc",
+                  "EDIT_TEMPLATE" => "",
+                  "PATH" => "/include/header/logo.php"
+                )
+              ); ?>
 
             </h1>
           </div>
-          
+
           <div class="col-4 col-md-4 col-lg-8">
-          
+
             <nav class="site-navigation text-right text-md-right" role="navigation">
-		          <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3">
-			          <a href="#" class="site-menu-toggle js-menu-toggle text-black">
-				          <span class="icon-menu h3"></span>
-			          </a>
-		          </div>
+              <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3">
+                <a href="#" class="site-menu-toggle js-menu-toggle text-black">
+                  <span class="icon-menu h3"></span>
+                </a>
+              </div>
 
-          <?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"top_menu", 
-	array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "left",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "3",
-		"MENU_CACHE_GET_VARS" => array(
-		),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "A",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "top",
-		"USE_EXT" => "N",
-		"COMPONENT_TEMPLATE" => "top_menu"
-	),
-	false
-);?>
+              <? $APPLICATION->IncludeComponent(
+                "bitrix:menu",
+                "top_multi",
+                array(
+                  "ALLOW_MULTI_SELECT" => "N",
+                  "CHILD_MENU_TYPE" => "left",
+                  "DELAY" => "N",
+                  "MAX_LEVEL" => "3",
+                  "MENU_CACHE_GET_VARS" => array(
+                    0 => "",
+                  ),
+                  "MENU_CACHE_TIME" => "3600",
+                  "MENU_CACHE_TYPE" => "N",
+                  "MENU_CACHE_USE_GROUPS" => "Y",
+                  "ROOT_MENU_TYPE" => "top",
+                  "USE_EXT" => "N",
+                ),
+                false
+              ); ?>
 
-</nav>
+            </nav>
 
 
 
@@ -169,71 +172,71 @@ $asset->addJs(SITE_TEMPLATE_PATH . "/js/main.js");
       </div>
     </div>
 
-		<? $GLOBALS['preferred'] = ['!PROPERTY_PREFERRED_DEAL' => false]; ?>
-	<? $APPLICATION->IncludeComponent(
-	"bitrix:news.list", 
-	"slider", 
-	array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "Y",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "N",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"DISPLAY_DATE" => "N",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "N",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(
-			0 => "",
-			1 => "",
-		),
-		"FILTER_NAME" => "preferred",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "5",
-		"IBLOCK_TYPE" => "ads",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"MESSAGE_404" => "",
-		"NEWS_COUNT" => "20",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Главная страница",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(
-			0 => "PRICE",
-			1 => "",
-		),
-		"SET_BROWSER_TITLE" => "Y",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_META_DESCRIPTION" => "Y",
-		"SET_META_KEYWORDS" => "Y",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "Y",
-		"SHOW_404" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC",
-		"STRICT_SECTION_CHECK" => "N",
-		"COMPONENT_TEMPLATE" => "slider"
-	),
-	false
-); ?>
+    <? $GLOBALS['preferred'] = ['!PROPERTY_PREFERRED_DEAL' => false]; ?>
+    <? $APPLICATION->IncludeComponent(
+      "bitrix:news.list",
+      "slider",
+      array(
+        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "ADD_SECTIONS_CHAIN" => "Y",
+        "AJAX_MODE" => "N",
+        "AJAX_OPTION_ADDITIONAL" => "",
+        "AJAX_OPTION_HISTORY" => "N",
+        "AJAX_OPTION_JUMP" => "N",
+        "AJAX_OPTION_STYLE" => "Y",
+        "CACHE_FILTER" => "N",
+        "CACHE_GROUPS" => "N",
+        "CACHE_TIME" => "36000000",
+        "CACHE_TYPE" => "A",
+        "CHECK_DATES" => "Y",
+        "DETAIL_URL" => "",
+        "DISPLAY_BOTTOM_PAGER" => "Y",
+        "DISPLAY_DATE" => "N",
+        "DISPLAY_NAME" => "Y",
+        "DISPLAY_PICTURE" => "Y",
+        "DISPLAY_PREVIEW_TEXT" => "N",
+        "DISPLAY_TOP_PAGER" => "N",
+        "FIELD_CODE" => array(
+          0 => "",
+          1 => "",
+        ),
+        "FILTER_NAME" => "preferred",
+        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+        "IBLOCK_ID" => "5",
+        "IBLOCK_TYPE" => "ads",
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+        "INCLUDE_SUBSECTIONS" => "Y",
+        "MESSAGE_404" => "",
+        "NEWS_COUNT" => "20",
+        "PAGER_BASE_LINK_ENABLE" => "N",
+        "PAGER_DESC_NUMBERING" => "N",
+        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+        "PAGER_SHOW_ALL" => "N",
+        "PAGER_SHOW_ALWAYS" => "N",
+        "PAGER_TEMPLATE" => ".default",
+        "PAGER_TITLE" => "Главная страница",
+        "PARENT_SECTION" => "",
+        "PARENT_SECTION_CODE" => "",
+        "PREVIEW_TRUNCATE_LEN" => "",
+        "PROPERTY_CODE" => array(
+          0 => "PRICE",
+          1 => "",
+        ),
+        "SET_BROWSER_TITLE" => "Y",
+        "SET_LAST_MODIFIED" => "N",
+        "SET_META_DESCRIPTION" => "Y",
+        "SET_META_KEYWORDS" => "Y",
+        "SET_STATUS_404" => "N",
+        "SET_TITLE" => "Y",
+        "SHOW_404" => "N",
+        "SORT_BY1" => "ACTIVE_FROM",
+        "SORT_BY2" => "SORT",
+        "SORT_ORDER1" => "DESC",
+        "SORT_ORDER2" => "ASC",
+        "STRICT_SECTION_CHECK" => "N",
+        "COMPONENT_TEMPLATE" => "slider"
+      ),
+      false
+    ); ?>
 
   </div>
