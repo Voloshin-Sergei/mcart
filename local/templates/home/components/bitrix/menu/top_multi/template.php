c6af67f193abd9679dbe346595f7f942ec51e7ff
Voloshin-Sergei <4voloshin@gmail.com>
2023-03-09 01:09:10 +0300

    Task 3 Правка вывода элементов меню

--- local/templates/home/components/bitrix/menu/top_multi/template.php
+++ local/templates/home/components/bitrix/menu/top_multi/template.php
@@ -8,55 +8,40 @@
 
     <?
     $previousLevel = 0;
-    foreach ($arResult as $arItem): ?>
 
-      <? if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel): ?>
-        <?= str_repeat("</ul></li>", ($previousLevel - $arItem["DEPTH_LEVEL"])); ?>
-      <? endif ?>
+    foreach($arResult as $arItem):?>
 
-      <? if ($arItem["IS_PARENT"]): ?>
+      <? if ($arItem["PERMISSION"] === "D") {
+          continue;
+      }?>
 
-        <? if ($arItem["DEPTH_LEVEL"] == 1): ?>
-          <li class="has-children <? if ($arItem["SELECTED"]): ?> active <? endif ?>">
-            <a href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a>
-            <ul class="dropdown">
-            <? else: ?>
-              <li class="has-children <? if ($arItem["SELECTED"]): ?> active <? endif ?>">
-                <a href="<?= $arItem["LINK"] ?>">
-                  <?= $arItem["TEXT"] ?>
-                </a>
-                <ul class="dropdown">
-                <? endif ?>
+    <?if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel):?>
+      <?=str_repeat("</ul></li>", ($previousLevel - $arItem["DEPTH_LEVEL"]));?>
+    <?endif?>
+  
+    <?if ($arItem["IS_PARENT"]):?>
+  
+      <?if ($arItem["DEPTH_LEVEL"] == 1):?>
+        <li class="has-children"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
+          <ul class="dropdown">
+      <?else:?>
+        <li class="has-children"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
+          <ul class="dropdown">
+      <?endif?>
+  
+    <?else:?>
 
-              <? else: ?>
-
-                <? if ($arItem["PERMISSION"] > "D"): ?>
-
-                  <? if ($arItem["DEPTH_LEVEL"] == 1): ?>
-                    <li><a href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a>
-                    </li>
-                  <? else: ?>
-                    <li class="<? if ($arItem["SELECTED"]): ?> active <? endif ?>">
-                      <a href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a>
-                    </li>
-                  <? endif ?>
-
-                <? else: ?>
-
-                  <? if ($arItem["DEPTH_LEVEL"] == 1): ?>
-                    <li><a href="" title="<?= GetMessage("MENU_ITEM_ACCESS_DENIED") ?>"><?= $arItem["TEXT"] ?></a></li>
-                  <? else: ?>
-                    <li><a href="" class="denied" title="<?= GetMessage("MENU_ITEM_ACCESS_DENIED") ?>"><?= $arItem["TEXT"] ?></a>
-                    </li>
-                  <? endif ?>
-
-                <? endif ?>
-
-              <? endif ?>
-
-              <? $previousLevel = $arItem["DEPTH_LEVEL"]; ?>
-
-            <? endforeach ?>
+      <?if ($arItem["DEPTH_LEVEL"] == 1):?>
+        <li><a href="<?=$arItem["LINK"]?>" class="<?if ($arItem["SELECTED"]):?>active<?endif?>"><?=$arItem["TEXT"]?></a></li>
+      <?else:?>
+        <li <? if ($arItem["SELECTED"]):?> class="active"<?endif?>><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+      <?endif?>
+      
+    <?endif?>
+  
+    <?$previousLevel = $arItem["DEPTH_LEVEL"];?>
+  
+  <?endforeach?>
 
             <? if ($previousLevel > 1): //close last item tags?>
               <?= str_repeat("</ul></li>", ($previousLevel - 1)); ?>
